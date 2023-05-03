c5314cdeced22694c18d7e233ccb30e7b3240a81
gudurred <gudurred@LIN76003684.corp.capgemini.com>
2023-05-03 09:53:26 +0530

    Commiting new code may-03

---
+++ src/gitKT/ExcelFileReader.java
@@ -0,0 +1,17 @@
+package gitKT;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExcelFileReader {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		
+		File file = new File("");
+		FileInputStream fis = new FileInputStream(file);
+
+	}
+
+}
