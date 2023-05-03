9140df96c95c7ebca3c738a58bef9be841d8c0e8
gudurred <gudurred@LIN76003684.corp.capgemini.com>
2023-05-03 11:11:16 +0530

    changes file name to text file reader

--- src/gitKT/TextFileReader.java
+++ src/gitKT/TextFileReader.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class ExcelFileReader {
+public class TextFileReader {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
